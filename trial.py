3d4e96ac19c57ef0070c1b86f27612e8415fa22a
naga-pramod <dpramod98@gmail.com>
2018-04-02 22:09:37 +0530

    added the main python file

---
+++ trial.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+browser = webdriver.Firefox()
+browser.get("http://krc.gitam.edu/papers")
+campus_selection = browser.find_element_by_id("ddlcampus").click()
+vizag_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div/div[2]/select/option[2]").click()
+college_selector = browser.find_element_by_id("ddlcollege").click()
+collegeselector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div/div[4]/select/option[11]").click()
+degree_clicker = browser.find_element_by_id("ddldegree").click()
+degree_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div/div[6]/select/option[2]").click()
+branch_clicker = browser.find_element_by_id("ddlbranch").click()
+branch_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[1]/div[8]/select/option[4]").click()
+year_clicker = browser.find_element_by_id("ddlyear").click()
+year_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[1]/div[10]/select/option[3]").click()
+acayear_clicker = browser.find_element_by_id("ddlacyear").click()
+acayear_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[1]/div[12]/select/option[10]").click()
+semester_clicker = browser.find_element_by_id("ddlsem").click()
+semester_selector = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[1]/div[14]/select/option[5]").click()
+view_clicker = browser.find_element_by_xpath("//*[@id='submit']").click()
+subject_1 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[1]/li/div[3]/a").click()
+subject_2 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[2]/li/div[3]/a").click()
+subject_3 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[3]/li/div[3]/a").click()
+subject_4 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[4]/li/div[3]/a").click()
+subject_5 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[5]/li/div[3]/a").click()
+subject_6 = browser.find_element_by_xpath("/html/body/section/form/div/div/div[2]/div[2]/ul[6]/li/div[3]/a").click()
